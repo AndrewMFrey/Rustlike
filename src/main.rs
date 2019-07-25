f596e05206641df643e3687ce73b65ce74883c4f
Andrew Frey <andrewmarkfrey@gmail.com>
2019-07-25 06:37:13 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+// actual size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// 20 FPS max
+const LIMIT_FPS: i32 = 20;
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        // Alt + Enter: toggle fullscreen
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        // exit game
+        Key { code: Escape, .. } => return true,
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {},
+    }
+
+    false
+}
+
+fn main() {
+    let mut root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT /2;
+
+    while !root.window_closed() {
+        root.set_default_foreground(WHITE);
+        root.clear();
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        root.flush();
+
+        // handle keys and exit game if needed
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+    }
+}
