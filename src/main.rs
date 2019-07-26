a1beca60224f7139d3382605b10c8ac34045c89e
Andrew Frey <andrewmarkfrey@gmail.com>
2019-07-26 00:21:20 -0700

    Added procedural generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::cmp;
+
+use rand::Rng;
+
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -8,7 +12,22 @@ const SCREEN_HEIGHT: i32 = 50;
 // 20 FPS max
 const LIMIT_FPS: i32 = 20;
 
+// playable map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100};
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150};
+
+// room restrictions
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+
+// map definition (2D vector containing tiles)
+type Map = Vec<Vec<Tile>>;
+
 // object for tracking characters
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
@@ -21,9 +40,11 @@ impl Object {
         Object { x, y, char, color }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     // set the color, then draw the character that represents this object at its position
@@ -33,7 +54,88 @@ impl Object {
     }
 }
 
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+// object for tracking rectangular rooms
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
+}
+
+// object for map tiles
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true
+        }
+    }
+}
+
+// add horizontal tunnel
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+// add room to map
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+// add vertical tunnel
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+// key handling
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -51,15 +153,99 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
         // exit game
         Key { code: Escape, .. } => return true,
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, &map),
+        Key { code: Down, .. } => player.move_by(0, 1, &map),
+        Key { code: Left, .. } => player.move_by(-1, 0, &map),
+        Key { code: Right, .. } => player.move_by(1, 0, &map),
 
         _ => {},
     }
 
     false
+}
+
+// map generation
+fn make_map() -> (Map, (i32, i32)) {
+    // fill with "blocked" tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let mut rooms = vec![];
+
+    let mut starting_position = (0, 0);
+
+    for _ in 0..MAX_ROOMS {
+        // random width and height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // keep location of room within map boundaries
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+        // create new room using above
+        let new_room = Rect::new(x, y, w, h);
+        // iterate through current list of rooms and check for collisions
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+        // no collisions, so valid room
+        if !failed {
+            // generate actual room and paint to map
+            create_room(new_room, &mut map);
+            // store center coordinates
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                starting_position = (new_x, new_y);
+            } else {
+                // all rooms beyond the first
+                // create connection with tunnel
+
+                // tie it to previous room
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // flip a coin
+                if rand::random() {
+                    // move horizontally, then vertically
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // generate the other way
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            // lastly, append the room to the list
+            rooms.push(new_room)
+        }
+    }
+
+    (map, starting_position)
+}
+
+// render all of the things
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    // draw everything
+    for object in objects {
+        object.draw(con);
+    }
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);            }
+        }
+    }
+
+    blit(con,
+         (0, 0),
+         (MAP_WIDTH, MAP_HEIGHT),
+         root,
+         (0, 0),
+         1.0,
+         1.0);
 }
 
 fn main() {
@@ -70,11 +256,12 @@ fn main() {
         .title("Rust/libtcod tutorial")
         .init();
 
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let (map, (player_x, player_y)) = make_map();
+    let player = Object::new(player_x, player_y, '@', WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '#', YELLOW);
     let mut objects = [player, npc];
 
@@ -84,19 +271,13 @@ fn main() {
         for object in &objects {
             object.draw(&mut con);
         }
-        blit(&mut con,
-             (0, 0),
-             (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut root,
-             (0, 0),
-            1.0,
-            1.0);
+        render_all(&mut root, &mut con, &mut objects, &map);
 
         root.flush();
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
-        let exit = handle_keys(&mut root, player);
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
         }
